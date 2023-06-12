2c3a12c9126a950b814e6c4f431b1f78fb0fa64a
Daniela Castillo <dcastillo5@uc.cl>
2023-06-12 12:53:08 -0400

    refact store

--- pucterest/src/store/images.js
+++ pucterest/src/store/images.js
@@ -3,26 +3,31 @@ import { writable } from "svelte/store";
 export const loading = writable(false);
 export const error = writable(false);
 export const data = writable([]);
+
 let url = "https://api.unsplash.com/photos/random?count=30";
 
 export const changeUrl = (newUrl) => {
   url = newUrl;
 };
+
+const fechtData = async () => {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: 
+      //"Client-ID MWAxqOZG1EXPZkFrnlWZ4PyB-2aqQZpEZydGHRmjDJI",
+      //"Client-ID 6H1smp1FpNFz-V7LF-BHILxuuM6MYd1aC_2lFqNN_2k",
+      "Client-ID nwcUw1WtMWgLsEeQQG_f8osbotd9JvJLJdPuVLnSsEw",
+    },
+  });
+  return await response.json();
+};
+
 export const get = async () => {
   loading.set(true);
   error.set(false);
-  console.info("get", url);
   try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: 
-        "Client-ID MWAxqOZG1EXPZkFrnlWZ4PyB-2aqQZpEZydGHRmjDJI",
-        //"Client-ID 6H1smp1FpNFz-V7LF-BHILxuuM6MYd1aC_2lFqNN_2k",
-        //"Client-ID nwcUw1WtMWgLsEeQQG_f8osbotd9JvJLJdPuVLnSsEw",
-      },
-    });
-    const imageData = await response.json();
+    const imageData = await fechtData();
     data.set(imageData);
   } catch (e) {
     error.set(e);
@@ -32,16 +37,7 @@ export const get = async () => {
 
 export const loadMore = async () => {
   try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: 
-        "Client-ID MWAxqOZG1EXPZkFrnlWZ4PyB-2aqQZpEZydGHRmjDJI",
-        //"Client-ID 6H1smp1FpNFz-V7LF-BHILxuuM6MYd1aC_2lFqNN_2k",
-        //"Client-ID nwcUw1WtMWgLsEeQQG_f8osbotd9JvJLJdPuVLnSsEw",
-      },
-    });
-    const imageData = await response.json();
+    const imageData = await fechtData();
     data.update((d) => {
       return [...d, ...imageData];
     });
